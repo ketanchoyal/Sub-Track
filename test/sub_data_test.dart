c7ef53944d55d1f17e85dd9a8565eac1c0a2a86c
Ketan Choyal <31490174+ketanchoyal@users.noreply.github.com>
2021-03-20 20:43:55 -0400

    Icons json added from simple-icons package

--- test/sub_data_test.dart
+++ test/sub_data_test.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 import 'package:sub_track/ui/resources/resources.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('sub_data assets test', () {
-    expect(true, File(SubData.imagesData).existsSync());
+    expect(true, File(SubData.iconss).existsSync());
     expect(true, File(SubData.categoryIcon).existsSync());
   });
 }

a49a522105e0c106b52f44cccc02c3c9de96bef9
Ketan Choyal <31490174+ketanchoyal@users.noreply.github.com>
2021-03-25 01:04:25 -0400

    Subscription repo abstract class added

--- lib/core/repository/subscription/subscription_repo.dart
+++ lib/core/repository/subscription/subscription_repo.dart
@@ -1,7 +1,9 @@
 import 'package:sub_track/core/models/subscription/subscription.dart';
 
 abstract class SubscriptionRepo {
-  fetchSubscriptions();
+  late List<Subscription> _subscriptions;
+  List<Subscription> get subscriptions => _subscriptions;
+  fetchSubscriptions({bool forceFetch = false});
   addSubscription({Subscription subsription});
   deleteSubscription({String subscriptionId});
   updateSubscription({Subscription subsription});

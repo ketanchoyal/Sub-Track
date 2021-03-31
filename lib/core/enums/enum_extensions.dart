6b18d7933e40112f8cd2eb714139351aadcf479b
Ketan Choyal <31490174+ketanchoyal@users.noreply.github.com>
2021-03-31 02:59:04 -0400

    Enum Extensions

---
+++ lib/core/enums/enum_extensions.dart
@@ -0,0 +1,136 @@
+part of "enums.dart";
+
+// SECTION ENUM
+extension RenewsEveryX on RenewsEvery {
+  String get value {
+    switch (this) {
+      case RenewsEvery.Never:
+        return "Never";
+      case RenewsEvery.Daily:
+        return "Daily";
+      case RenewsEvery.Weekly:
+        return "Weekly";
+      case RenewsEvery.Biweekly:
+        return "Bi-Weekly";
+      case RenewsEvery.Monthly:
+        return "Monthly";
+      case RenewsEvery.Quarterly:
+        return "Quarterly";
+      case RenewsEvery.Half_yearly:
+        return "Half-Yearly";
+      case RenewsEvery.Yearly:
+        return "Yearly";
+    }
+  }
+
+  String get initial {
+    switch (this) {
+      case RenewsEvery.Never:
+        return "";
+      case RenewsEvery.Daily:
+        return "day";
+      case RenewsEvery.Weekly:
+        return "week";
+      case RenewsEvery.Biweekly:
+        return "biweek";
+      case RenewsEvery.Monthly:
+        return "m";
+      case RenewsEvery.Quarterly:
+        return "quat";
+      case RenewsEvery.Half_yearly:
+        return "halfyear";
+      case RenewsEvery.Yearly:
+        return "yr";
+    }
+  }
+}
+
+extension NotifyOnX on NotifyOn {
+  String get value {
+    switch (this) {
+      case NotifyOn.Never:
+        return "Never";
+      case NotifyOn.Same_Day:
+        return "Same Day";
+      case NotifyOn.One_Day_Before:
+        return "One Day Before";
+      case NotifyOn.One_Week_Before:
+        return "One Week Before";
+    }
+  }
+}
+
+extension IconTypeX on IconType {
+  String get value {
+    switch (this) {
+      case IconType.SVG:
+        return "svg";
+      case IconType.EMOJI:
+        return "emoji";
+      case IconType.NONE:
+        return "none";
+    }
+  }
+}
+
+// !SECTION
+
+// SECTION String
+
+extension EnumString on String? {
+  /// RenewsEvery enum from String
+  RenewsEvery get enumRE {
+    switch (this) {
+      case "Never":
+        return RenewsEvery.Never;
+      case "Daily":
+        return RenewsEvery.Daily;
+      case "Weekly":
+        return RenewsEvery.Weekly;
+      case "Bi-Weekly":
+        return RenewsEvery.Biweekly;
+      case "Monthly":
+        return RenewsEvery.Monthly;
+      case "Quarterly":
+        return RenewsEvery.Quarterly;
+      case "Half-Yearly":
+        return RenewsEvery.Half_yearly;
+      case "Yearly":
+        return RenewsEvery.Yearly;
+      default:
+        return RenewsEvery.Never;
+    }
+  }
+
+  /// NotifyOn enum from String
+  NotifyOn get enumNO {
+    switch (this) {
+      case "Never":
+        return NotifyOn.Never;
+      case "Same Day":
+        return NotifyOn.Same_Day;
+      case "One Day Before":
+        return NotifyOn.One_Day_Before;
+      case "One Week Before":
+        return NotifyOn.One_Week_Before;
+      default:
+        return NotifyOn.Never;
+    }
+  }
+
+  /// IconType enum from String
+  IconType get enumIT {
+    switch (this) {
+      case "svg":
+        return IconType.SVG;
+      case "emoji":
+        return IconType.EMOJI;
+      case "none":
+        return IconType.NONE;
+      default:
+        return IconType.NONE;
+    }
+  }
+}
+
+// !SECTION
--- lib/core/enums/enums.dart
+++ lib/core/enums/enums.dart
@@ -1,3 +1,5 @@
+part "enum_extensions.dart";
+
 enum RenewsEvery {
   Never,
   Daily,
@@ -9,50 +11,6 @@ enum RenewsEvery {
   Yearly,
 }
 
-extension RenewsEveryX on RenewsEvery {
-  String get value {
-    switch (this) {
-      case RenewsEvery.Never:
-        return "Never";
-      case RenewsEvery.Daily:
-        return "Daily";
-      case RenewsEvery.Weekly:
-        return "Weekly";
-      case RenewsEvery.Biweekly:
-        return "Bi-Weekly";
-      case RenewsEvery.Monthly:
-        return "Monthly";
-      case RenewsEvery.Quarterly:
-        return "Quarterly";
-      case RenewsEvery.Half_yearly:
-        return "Half-Yearly";
-      case RenewsEvery.Yearly:
-        return "Yearly";
-    }
-  }
-
-  String get initial {
-    switch (this) {
-      case RenewsEvery.Never:
-        return "";
-      case RenewsEvery.Daily:
-        return "day";
-      case RenewsEvery.Weekly:
-        return "week";
-      case RenewsEvery.Biweekly:
-        return "biweek";
-      case RenewsEvery.Monthly:
-        return "m";
-      case RenewsEvery.Quarterly:
-        return "quat";
-      case RenewsEvery.Half_yearly:
-        return "halfyear";
-      case RenewsEvery.Yearly:
-        return "yr";
-    }
-  }
-}
-
 enum NotifyOn {
   Never,
   Same_Day,
@@ -60,22 +18,8 @@ enum NotifyOn {
   One_Week_Before,
 }
 
-extension NotifyOnX on NotifyOn {
-  String get value {
-    switch (this) {
-      case NotifyOn.Never:
-        return "Never";
-      case NotifyOn.Same_Day:
-        return "Same Day";
-      case NotifyOn.One_Day_Before:
-        return "One Day Before";
-      case NotifyOn.One_Week_Before:
-        return "One Week Before";
-    }
-  }
-}
-
 enum IconType {
   SVG,
   EMOJI,
+  NONE,
 }

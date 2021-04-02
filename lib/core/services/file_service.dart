ad70a880aa623f8c1789850a6f7229b64e4b84b7
Ketan Choyal <31490174+ketanchoyal@users.noreply.github.com>
2021-04-02 18:25:35 -0400

    Added file_service.dart

---
+++ lib/core/services/file_service.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:image_picker/image_picker.dart';
+
+abstract class FileService {
+  Future<String> getApplicationDocumentsDirectoryPath();
+  Future<File?> getImage({bool fromGallery = true});
+}
+
+class FileServiceImpl implements FileService {
+  @override
+  Future<String> getApplicationDocumentsDirectoryPath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  @override
+  Future<File?> getImage({bool fromGallery = true}) async {
+    ImagePicker imagePicker = new ImagePicker();
+    PickedFile? pickedFile = await imagePicker.getImage(
+        source: fromGallery ? ImageSource.gallery : ImageSource.camera);
+    if (pickedFile != null) return File(pickedFile.path);
+    return null;
+  }
+}

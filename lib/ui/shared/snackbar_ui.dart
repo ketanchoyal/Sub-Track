35859a4121cecfc809a418e8ccf2db5042146126
ketanchoyal <sharmaketanr@gmail.com>
2021-04-15 15:18:05 -0400

    SnackBar added

---
+++ lib/ui/shared/snackbar_ui.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/widgets.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:sub_track/app/app.locatorx.dart';
+import 'package:sub_track/ui/theme/app_colors.dart';
+import 'package:get/get.dart';
+
+void setupSnackbarUi() {
+  final service = locator<SnackbarService>();
+
+  // Registers a config to be used when calling showSnackbar
+  // service.registerSnackbarConfig(SnackbarConfig(
+  //   backgroundColor: AppColors.error,
+  //   animationDuration: Duration(milliseconds: 400),
+  //   isDismissible: true,
+  //   maxWidth: double.infinity,
+  //   snackPosition: SnackPosition.TOP,
+  // ));
+
+  service.registerCustomSnackbarConfig(
+    variant: SnackbarType.Success,
+    config: SnackbarConfig(
+      isDismissible: true,
+      animationDuration: Duration(seconds: 1),
+      backgroundColor: AppColor.STSuccess,
+      textColor: AppColor.STPureWhite,
+      borderRadius: 10,
+      forwardAnimationCurve: Curves.bounceIn,
+      // reverseAnimationCurve: Curves.bounceOut,
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.all(15),
+      snackPosition: SnackPosition.BOTTOM,
+      snackStyle: SnackStyle.FLOATING,
+      dismissDirection: SnackDismissDirection.HORIZONTAL,
+    ),
+  );
+
+  service.registerCustomSnackbarConfig(
+    variant: SnackbarType.Error,
+    config: SnackbarConfig(
+      isDismissible: true,
+      backgroundColor: AppColor.STFailure,
+      animationDuration: Duration(seconds: 1),
+      textColor: AppColor.STPureWhite,
+      borderRadius: 10,
+      margin: EdgeInsets.all(15),
+      forwardAnimationCurve: Curves.bounceIn,
+      // reverseAnimationCurve: Curves.bounceOut,
+      padding: EdgeInsets.all(15),
+      snackPosition: SnackPosition.BOTTOM,
+      dismissDirection: SnackDismissDirection.HORIZONTAL,
+    ),
+  );
+}
+
+enum SnackbarType { Success, Error }

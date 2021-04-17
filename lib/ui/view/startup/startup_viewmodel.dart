31eea8851f061bfb0e5f4d70a6e77f0882f83499
ketanchoyal <sharmaketanr@gmail.com>
2021-04-17 13:24:25 -0400

    Fixed googlesignin for ios and move to home if loggedin added

--- lib/ui/view/startup/startup_viewmodel.dart
+++ lib/ui/view/startup/startup_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:sub_track/app/app.locatorx.dart';
 import 'package:sub_track/app/app.router.dart';
-import 'package:sub_track/core/data_source/brand/brand_abstract.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:sub_track/core/data_source/brand/brand_local.dart';
 import 'package:sub_track/core/data_source/subscription/sub_local.dart';
 import 'package:sub_track/core/services/notification_service.dart';
@@ -15,6 +15,16 @@ class StartUpViewModel extends BaseViewModel {
     await locator<NotificationService>().init();
     await locator<BrandLocalDataSource>().init();
     await locator<SubscriptionLocalDataSource>().init();
-    $navigationService.replaceWith(Routes.onBoardingView);
+    _checkIfLoggedIn();
+  }
+
+  _checkIfLoggedIn() async {
+    final firebaseAuthenticationService =
+        locator<FirebaseAuthenticationService>();
+
+    if (firebaseAuthenticationService.hasUser)
+      $navigationService.clearStackAndShow(Routes.homeView);
+    else
+      $navigationService.replaceWith(Routes.onBoardingView);
   }
 }

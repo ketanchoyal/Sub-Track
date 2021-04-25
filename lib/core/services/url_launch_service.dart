14c0fffd49a1de4850ecd24f73eb8ca5117ad267
ketanchoyal <sharmaketanr@gmail.com>
2021-04-24 21:43:36 -0400

    URL launch service added

---
+++ lib/core/services/url_launch_service.dart
@@ -0,0 +1,32 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UrlLaunchService {
+  Future makePhoneCall(String phoneNumber) async =>
+      await _makePhoneCall(phoneNumber);
+  Future sendSms(String phoneNumber) async => await _sms(phoneNumber);
+  Future launchUrl(String url) async => await _launchUrl(url);
+
+  Future<void> _makePhoneCall(String url) async {
+    if (await canLaunch('tel: $url')) {
+      await launch('tel:+ $url');
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _sms(String url) async {
+    if (await canLaunch('sms:$url')) {
+      await launch('sms:$url');
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
